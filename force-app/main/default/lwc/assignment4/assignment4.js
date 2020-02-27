5062bf611f9ddac9beb4f1b62267a09e32d334ca
BRI AGUILAR <briaguilar@bris-air.home>
2020-02-27 11:16:46 -0600

    Added apex 3 apex classes to call the tasks, events, list emails from the database. Updated JS and HTML to accomodate

--- force-app/main/default/lwc/assignment4/assignment4.js
+++ force-app/main/default/lwc/assignment4/assignment4.js
@@ -1,25 +1,91 @@
-import { LightningElement, api, wire, track } from 'lwc';
+import { LightningElement, wire, track } from 'lwc';
 import getTasks from '@salesforce/apex/ActivityLeadPage.getTasks';
+import getEvents from '@salesforce/apex/EventsLeadPage.getEvents';
+import getListEmails from '@salesforce/apex/ListEmails.getListEmails';
 
-const columns = [
-    { label: 'Task', fieldName: 'TaskSubtype'},
-    { label: 'Email', fieldName: 'Email' },
+
+
+const taskColumns = [
+    { label: 'Task', fieldName: 'TaskSubtype' },
+    { label: 'Assigned To', fieldName: 'OwnerId' },
     { label: 'Subject', fieldName: 'Subject' },
     { label: 'Name', fieldName: 'WhoId' },
-    { label: 'Status', fieldName: 'Status'}
+    { label: 'Status', fieldName: 'Status' }
 ];
 
-export default class Assignment4 extends LightningElement {
-    @api recordId;
-    @track data = [];
-    @track columns = columns;
 
-    @wire(getTasks, {})
-    ApexResponse({ error, data }) {
+const eventColumns = [
+    { label: 'Event', fieldName: 'EventSubtype' },
+    { label: 'Assigned To', fieldName: 'OwnerId' },
+    { label: 'Subject', fieldName: 'Subject' },
+    { label: 'Description', fieldname: 'Description' },
+    { label: 'Name', fieldName: 'WhoId' },
+    { label: 'Activity Date', fieldName: 'ActivityDate' }
+];
+
+
+const listEmailColumns = [
+    { label: 'From', fieldName: 'FromName' },
+    { label: 'Subject', fieldName: 'Subject' },
+    { label: 'Date Received', fieldName: 'MessageDate' },
+    { label: 'Status', fieldName: 'Status' }
+];
+
+
+
+export default class Assignment4 extends LightningElement {
+    @track eventData = [];
+    @track eventColumns = eventColumns;
+
+    @wire(getEvents)
+    wiredEvents({ error, data }) {
         if (data) {
-            this.data = data;
+            this.eventData = data;
         } else if (error) {
-            // test
+            this.error = error;
         }
     }
+
+
+
+    @track taskData = [];
+    @track taskColumns = taskColumns;
+
+    @wire(getTasks)
+    wiredTasks({ error, data }) {
+        if (data) {
+            this.taskData = data;
+        } else if (error) {
+            this.error = error;
+        }
+    }
+
+
+    @track listEmailData = [];
+    @track listEmailColumns = listEmailColumns;
+
+    @wire(getListEmails)
+    wiredlistEmails({ error, data }) {
+        if (data) {
+            this.listEmailData = data;
+        } else if (error) {
+            this.error = error;
+        }
+    }
+
+
+    // This works but just for the template section
+    // tasks;
+    // error;
+
+    // @wire(getTasks)
+    // wiredTasks({ error, data }) {
+    //     if (data) {
+    //         this.tasks = data;
+    //         this.error = undefined;
+    //     } else if (error) {
+    //         this.error = error;
+    //         this.tasks = undefined;
+    //     }
+    // }
 }
